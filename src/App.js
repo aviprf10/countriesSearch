0c9d6956e1eec064d2ee7934f9901774e3417620
Avinash Tiwari <aviprf10@gmail.com>
2023-12-25 19:52:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const CountryList = () => {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    // Fetch data from the API on initial render
+    fetchCountries();
+  }, []);
+
+  const fetchCountries = async () => {
+    try {
+      const response = await fetch('https://restcountries.com/v3.1/all');
+      const data = await response.json();
+      setCountries(data);
+      setFilteredCountries(data);
+    } catch (error) {
+      console.error('Error fetching countries:', error);
+    }
+  };
+
+  const handleSearch = (e) => {
+    const term = e.target.value.toLowerCase();
+    setSearchTerm(term);
+
+    // Filter countries based on the partial search term
+    const filtered = countries.filter(
+      (country) =>
+        country.name.common.toLowerCase().includes(term) ||
+        country.name.official.toLowerCase().includes(term)
+    );
+    setFilteredCountries(filtered);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Country List</h1>
+      <input
+        type="text"
+        placeholder="Search countries..."
+        value={searchTerm}
+        onChange={handleSearch}
+      />
+      <div className="country-list">
+        {filteredCountries.length === 0 ? (
+          <p>No matching countries found</p>
+        ) : (
+          filteredCountries.map((country) => (
+            <div key={country.cca3} className='country-card'>
+              <img
+                src={country.flags.png}
+                alt={`${country.name.common} Flag`}
+                style={{ width: '100%', height: '85%' }}
+              />
+              <span>{country.name.common}</span>
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default CountryList;
